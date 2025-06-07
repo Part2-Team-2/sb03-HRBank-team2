e3be3e0aa80912a624c76ac8d340de1f8eb3786a
GeonMin02 <geonmin10@gmail.com>
2025-06-07 14:40:05 +0900

    refactor: ChangeLogDiff 관계 매핑 추가

--- HRBank/src/main/java/org/yebigun/hrbank/domain/changelog/entity/ChangeLog.java
+++ HRBank/src/main/java/org/yebigun/hrbank/domain/changelog/entity/ChangeLog.java
@@ -1,14 +1,18 @@
 package org.yebigun.hrbank.domain.changelog.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.Instant;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -41,5 +45,8 @@ public class ChangeLog {
 
     @Column(name = "at", nullable = false, columnDefinition = "TIMESTAMPTZ")
     private Instant at;
+
+    @OneToMany(mappedBy = "changeLog", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ChangeLogDiff> diffs;
 
 }
