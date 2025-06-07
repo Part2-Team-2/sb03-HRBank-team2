3dc0f2678dfd0310dd5ac8cd92a17cbfc3300689
김동욱 Daniel Kim <dongwook5478@gmail.com>
2025-06-07 11:14:13 +0900

    fix: null 체크가 없어서 ip가 누락될 수 있는 문제 수정

--- HRBank/src/main/java/org/yebigun/hrbank/domain/backup/service/BackupServiceImpl.java
+++ HRBank/src/main/java/org/yebigun/hrbank/domain/backup/service/BackupServiceImpl.java
@@ -109,19 +109,19 @@ public class BackupServiceImpl implements BackupService {
 
         if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
-            if(isValidIp(ip)) return ip;
+            if (ip != null && isValidIp(ip)) return ip;
         }
         if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
-            if(isValidIp(ip)) return ip;
+            if(ip != null && isValidIp(ip)) return ip;
         }
         if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
-            if(isValidIp(ip)) return ip;
+            if(ip != null && isValidIp(ip)) return ip;
         }
         if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-            if(isValidIp(ip)) return ip;
+            if(ip != null && isValidIp(ip)) return ip;
         }
         if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
