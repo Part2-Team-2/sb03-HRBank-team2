eb902eb796ad20d84742cb09a48257ff56c35ace
noonsong0208 <tmdwls7795@gmail.com>
2025-06-11 17:50:22 +0900

    refactor: 어노테이션 추가, 파일 삭제시 메타파일도 함께 제거, 디버그 출력문 제거

--- HRBank/src/main/java/org/yebigun/hrbank/domain/binaryContent/storage/BinaryContentStorageImpl.java
+++ HRBank/src/main/java/org/yebigun/hrbank/domain/binaryContent/storage/BinaryContentStorageImpl.java
@@ -16,8 +16,12 @@ import org.yebigun.hrbank.domain.binaryContent.repository.BinaryContentRepositor
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.*;
-import java.util.List;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -196,6 +200,7 @@ public class BinaryContentStorageImpl implements BinaryContentStorage {
 
         try {
             Files.deleteIfExists(pathToDelete);
+            binaryContentRepository.delete(binaryContent);
         } catch (IOException e) {
             throw new RuntimeException("파일 삭제 실패", e);
         }
