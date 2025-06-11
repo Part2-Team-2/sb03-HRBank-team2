76d2e4bc6fd779f1dd6589a3527180db59e79532
Dw-real <gksehddn8@naver.com>
2025-06-11 10:46:30 +0900

    refactor: Employee 패키지커스텀 예외 클래스 생성

--- HRBank/src/main/java/org/yebigun/hrbank/domain/employee/service/EmployeeServiceImpl.java
+++ HRBank/src/main/java/org/yebigun/hrbank/domain/employee/service/EmployeeServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.yebigun.hrbank.domain.department.entity.Department;
+import org.yebigun.hrbank.domain.department.exception.NotFoundDepartmentException;
 import org.yebigun.hrbank.domain.department.repository.DepartmentRepository;
 import org.yebigun.hrbank.domain.employee.dto.data.EmployeeDistributionDto;
 import org.yebigun.hrbank.domain.employee.dto.data.EmployeeDto;
@@ -18,6 +19,11 @@ import org.yebigun.hrbank.domain.employee.dto.request.EmployeeCreateRequest;
 import org.yebigun.hrbank.domain.employee.dto.request.EmployeeListRequest;
 import org.yebigun.hrbank.domain.employee.entity.Employee;
 import org.yebigun.hrbank.domain.employee.entity.EmployeeStatus;
+import org.yebigun.hrbank.domain.employee.exception.DuplicateEmailException;
+import org.yebigun.hrbank.domain.employee.exception.UnsupportedGroupByException;
+import org.yebigun.hrbank.domain.employee.exception.UnsupportedSortDirectionException;
+import org.yebigun.hrbank.domain.employee.exception.UnsupportedSortFieldException;
+import org.yebigun.hrbank.domain.employee.exception.UnsupportedUnitException;
 import org.yebigun.hrbank.domain.employee.mapper.EmployeeMapper;
 import org.yebigun.hrbank.domain.employee.repository.EmployeeRepository;
 import org.yebigun.hrbank.global.dto.CursorPageResponse;
@@ -38,7 +44,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<EmployeeTrendDto> getEmployeeTrend(LocalDate from, LocalDate to, String unit) {
 
         if (!VALID_UNIT.contains(unit)) {
-            throw new IllegalArgumentException("지원하지 않는 시간 단위입니다.");
+            throw new UnsupportedUnitException("지원하지 않는 시간 단위입니다.");
         }
 
         from = from != null ? from : getDate(unit);
@@ -54,7 +60,7 @@ public class EmployeeServiceImpl implements EmployeeService {
                                                                  EmployeeStatus status) {
 
         if (!VALID_GROUP_BY.contains(groupBy)) {
-            throw new IllegalArgumentException("지원하지 않는 그룹화 기준입니다.");
+            throw new UnsupportedGroupByException("지원하지 않는 그룹화 기준입니다.");
         }
 
         List<EmployeeDistributionDto> employees = employeeRepository.findEmployeeByStatusGroupByDepartmentOrPosition(
@@ -67,32 +73,12 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Transactional
     public EmployeeDto createEmployee(EmployeeCreateRequest request, MultipartFile profile) {
 
-        // record 방식: getXxx() → xxx()
-        if (request.name() == null || request.name().trim().isEmpty())
-            throw new IllegalArgumentException("이름은 필수입니다.");
-
-        if (request.email() == null || request.email().trim().isEmpty())
-            throw new IllegalArgumentException("이메일은 필수입니다.");
-
-        if (request.departmentId() == null)
-            throw new IllegalArgumentException("부서는 필수입니다.");
-
-        if (request.position() == null || request.position().trim().isEmpty())
-            throw new IllegalArgumentException("직급은 필수입니다.");
-
-        if (request.hireDate() == null)
-            throw new IllegalArgumentException("입사일은 필수입니다.");
-
-        if (!request.email().matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
-            throw new IllegalArgumentException("올바른 이메일 형식이 아닙니다.");
-        }
-
         if (employeeRepository.existsByEmail(request.email())) {
-            throw new IllegalArgumentException("이미 등록된 이메일입니다.");
+            throw new DuplicateEmailException("이미 등록된 이메일입니다.");
         }
 
         Department department = departmentRepository.findById(request.departmentId())
-            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 부서입니다."));
+            .orElseThrow(() -> new NotFoundDepartmentException("존재하지 않는 부서입니다."));
 
         String generatedEmpNo = generateUniqueEmployeeNumber();
 
@@ -138,7 +124,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         String sortDirection = request.sortDirection();
 
         if (!VALID_SORT_DIRECTION.contains(sortDirection)) {
-            throw new IllegalArgumentException("잘못된 정렬 방향입니다.");
+            throw new UnsupportedSortDirectionException("잘못된 정렬 방향입니다.");
         }
 
         List<Employee> employees = employeeRepository.findAllByRequest(
@@ -176,12 +162,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         return response;
     }
 
-    private String getNextCursor(String sortField, Employee employee) {
+    private String getNextCursor(String sortField, Employee employee)
+        throws UnsupportedSortFieldException {
         return switch (sortField) {
             case "name" -> employee.getName();
             case "employeeNumber" -> employee.getEmployeeNumber();
             case "hireDate" -> employee.getHireDate().toString();
-            default -> throw new IllegalArgumentException("지원하지 않는 정렬 필드입니다.");
+            default -> throw new UnsupportedSortFieldException("지원하지 않는 정렬 필드입니다.");
         };
     }
 
