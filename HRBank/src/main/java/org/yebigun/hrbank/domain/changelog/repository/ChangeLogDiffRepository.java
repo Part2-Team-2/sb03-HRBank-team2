051aca20c35cf5ee220aa692ca809cd16b9e5fd2
GeonMin02 <geonmin10@gmail.com>
2025-06-11 13:13:38 +0900

    refactor: ChangeLogDiff 레포지토리 어노테이션 설정

--- HRBank/src/main/java/org/yebigun/hrbank/domain/changelog/repository/ChangeLogDiffRepository.java
+++ HRBank/src/main/java/org/yebigun/hrbank/domain/changelog/repository/ChangeLogDiffRepository.java
@@ -1,8 +1,10 @@
 package org.yebigun.hrbank.domain.changelog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.yebigun.hrbank.domain.changelog.entity.ChangeLogDiff;
 
+@Repository
 public interface ChangeLogDiffRepository extends JpaRepository<ChangeLogDiff,Long> {
 
 }
